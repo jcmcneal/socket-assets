3631df9909f30a51478e3923d3f1790f71c53dd1
Jason McNeal <jmcneal@overstock.com>
2018-08-22 10:07:16 -0600

    Prototype - socket is slower right now

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// Utils
+const fs = require('fs');
+const path = require('path');
+const prettySize = require('prettysize');
+const now = require('performance-now');
+// Server Requirements
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -15,15 +21,48 @@ app.get('/', (req, res) => {
 // Socket Server
 io.on('connection', (socket) => {
     console.log('Client connected!');
-    socket.emit('message', { message: 'Welcome Yo! '})
+    socket.emit('message', { message: 'Welcome client! '})
 
-    socket.on('getAssets', (data) => {
-        console.log('Client requests asset', data);
-        socket.emit('asset', data);
+    socket.on('getAssets', (assets) => {
+        assets.forEach(asset => {
+            const file = path.join(__dirname, asset.src);
+            console.log('Client is requesting', file);
+            // Get stats
+            const stats = fs.statSync(file);
+
+            // If file doesn't exist
+            if (!stats) {
+                socket.emit('message', { message: "File can't be found", status: 404, asset});
+                return;
+            }
+
+            // Read file content
+            const startRead = now();
+            fs.readFile(file, 'utf8', (err, content) => {
+                if (err) {
+                    // File read failure
+                    socket.emit('message', { message: "Error reading file", status: 404, asset});
+                    return;
+                }
+
+                // Send content
+                socket.emit('asset', {
+                    ...asset,
+                    content,
+                    size: prettySize(stats.size),
+                    speed: `${(now() - startRead).toFixed(4)} ms`,
+                });
+            });
+        });
     });
 
     socket.on('end', () => {
+        socket.emit('message', { message: 'Goodbye! '})
         console.log('Client disconnected');
+    });
+
+    socket.on('message', (data) => {
+        console.log('Client message:', data);
     });
 });
 
